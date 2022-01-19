4083f976ca439b4ee7a981914ce835657377ec5b
corpuzrob <93008995+corpuzrob@users.noreply.github.com>
2022-01-18 21:00:50 -0800

    Kitten_Zen

---
+++ Kitten_Zen/JS/Kitten_Zen.js
@@ -0,0 +1,20 @@
+
+
+var x = document.getElementById("myAudio"); 
+var myimgobj = document.images["jsbutton"];
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+} 
+
+function myFunction() {
+    alert("Hello! I am an alert box!");
+  }
+
+  function playMyAudio(){
+    document.getElementById("otherAudio").play();
+  }
